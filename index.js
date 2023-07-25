262b4fd8745fdae516ca49275c49e5bc91b2e4fe
muhammadadeel547 <muhammad.adeel@deutics.com>
2023-07-25 16:20:40 +0500

    updated 2

--- index.js
+++ index.js
@@ -5,48 +5,71 @@ const lisptickMate = [
     icon: "/Images/lipstik/lipstick_img1_color.svg",
     title: "Powermatte Long Lasting",
     content: "Transfer-resistant, matte lipstick",
+    price: "$200",
   },
   {
-    id: 1,
+    id: 2,
+    img: "/Images/lipstik/lipstik_img4.svg",
+    icon: "/Images/lipstik/lipstick_img3_color.svg",
+    title: "Rouge Refillable Lipstock",
+    content: "Buildable lip colour with a blurred, soft-focus effect",
+    price: "$200",
+  },
+  {
+    id: 3,
+    img: "/Images/lipstik/lipstik_img2.svg",
+    icon: "/Images/lipstik/lipstick_img1_color.svg",
+    title: "Matte Refillable Lipstick ",
+    content: "Transfer-resistant, matte lipstick ",
+    price: "$200",
+  },
+  {
+    id: 4,
+    img: "/Images/lipstik/lipstik_img1.svg",
+    icon: "/Images/lipstik/lipstick_img1_color.svg",
+    title: "Velvet Matte Lipstick",
+    content: "Transfer-resistant, matte lipstick",
+    price: "$200",
+  },
+  {
+    id: 5,
     img: "/Images/lipstik/lipstik_img1.svg",
     icon: "/Images/lipstik/lipstick_img1_color.svg",
     title: "Powermatte Long Lasting",
     content: "Transfer-resistant, matte lipstick",
+    price: "$200",
   },
   {
-    id: 1,
+    id: 6,
     img: "/Images/lipstik/lipstik_img1.svg",
+    icon: "/Images/lipstik/lipstick_img1_color.svg",
+    title: "Kind Words Matte Lipstick",
+    content: "Transfer-resistant, matte lipstick",
+    price: "$200",
+  },
+  {
+    id: 7,
+    img: "/Images/lipstik/lipstik_img3.svg",
     icon: "/Images/lipstik/lipstick_img1_color.svg",
     title: "Powermatte Long Lasting",
     content: "Transfer-resistant, matte lipstick",
+    price: "$200",
   },
   {
-    id: 1,
-    img: "/Images/lipstik/lipstik_img1.svg",
+    id: 8,
+    img: "/Images/lipstik/lipstik_img2.svg",
     icon: "/Images/lipstik/lipstick_img1_color.svg",
-    title: "Powermatte Long Lasting",
+    title: "Kind Words Matte Lipstick",
     content: "Transfer-resistant, matte lipstick",
+    price: "$200",
   },
   {
-    id: 1,
+    id: 9,
     img: "/Images/lipstik/lipstik_img1.svg",
     icon: "/Images/lipstik/lipstick_img1_color.svg",
-    title: "Powermatte Long Lasting",
+    title: "Velvet Matte Lipstick",
     content: "Transfer-resistant, matte lipstick",
-  },
-  {
-    id: 1,
-    img: "/Images/lipstik/lipstik_img1.svg",
-    icon: "/Images/lipstik/lipstick_img1_color.svg",
-    title: "Powermatte Long Lasting",
-    content: "Transfer-resistant, matte lipstick",
-  },
-  {
-    id: 1,
-    img: "/Images/lipstik/lipstik_img1.svg",
-    icon: "/Images/lipstik/lipstick_img1_color.svg",
-    title: "Powermatte Long Lasting",
-    content: "Transfer-resistant, matte lipstick",
+    price: "$200",
   },
 ];
 
@@ -94,15 +117,19 @@ if (storedButton) {
   });
 }
 
-function showLipstickSection(sectionId, button) {
+function showLipstickSection(sectionId, button, element) {
   // Hide all sections
   const sections = document.querySelectorAll(".LipstickContent");
   sections.forEach((section) => {
     section.style.display = "none";
   });
+  const sections2 = document.querySelectorAll(".LipstickContent");
+  sections2.forEach((section) => {
+    section.style.display = "none";
+  });
 
   // Remove the "selected" class from all buttons
-  const buttons = document.querySelectorAll(".LipstickContainer a");
+  const buttons = document.querySelectorAll(".LipstickContainerCategories a");
   buttons.forEach((btn) => {
     btn.classList.remove("selectedlipstick");
   });
@@ -111,6 +138,7 @@ function showLipstickSection(sectionId, button) {
   const selectedSectionlipstick = document.getElementById(sectionId);
   if (selectedSectionlipstick) {
     selectedSectionlipstick.style.display = "flex";
+
     localStorage.setItem("selectedSectionlipstick", sectionId);
   }
 
@@ -131,7 +159,7 @@ if (storedSectionlipstick) {
 
 // Restore the selected buttons border after the page is refreshed
 if (storedButtonlipstick) {
-  const buttons = document.querySelectorAll(".LipstickContainer a");
+  const buttons = document.querySelectorAll(".LipstickContainerCategories a");
   buttons.forEach((btn) => {
     if (btn.getAttribute("href") === storedButtonlipstick) {
       btn.classList.add("selectedlipstick");
@@ -139,13 +167,121 @@ if (storedButtonlipstick) {
   });
 }
 
+// Helper function to display the selected lipstick in the addToCart section
+function displaySelectedLipstick(img, icon, title, price, index) {
+  const addToCartSection = document.querySelector(".addToCart");
+
+  // Create a new div to hold each selected lipstick
+  const selectedLipstickDiv = document.createElement("div");
+  selectedLipstickDiv.classList.add("selectedLipstickInfo");
+
+  // Populate the div with the selected lipstick data
+  selectedLipstickDiv.innerHTML = `
+    <div class="lipstickInfo">
+      <img class="addToCartImg" src="${img}" />
+      <img class="addToCartIcon" src="${icon}" />
+      <h4>${title}</h4>
+      <p>${price}</p>
+      <div class="deleteButton" data-index="${index}"><img class="delete_icon" src="/Images/Icons/detele_Icon.svg" /></div>
+      </div>
+     
+  `;
+
+  const deleteButton = selectedLipstickDiv.querySelector(".deleteButton");
+  deleteButton.addEventListener("click", () => {
+    // Get the index of the selected lipstick from the data-index attribute
+    const indexToRemove = parseInt(deleteButton.getAttribute("data-index"), 10);
+
+    // Call the function to remove the selected lipstick from the array and update the display
+    removeSelectedLipstick(selectedLipstickDiv, indexToRemove);
+  });
+
+  // Append the div to the addToCartSection
+  addToCartSection.appendChild(selectedLipstickDiv);
+}
+let selectedLipsticksArray = [];
+
+function removeSelectedLipstick(selectedLipstickDiv, indexToRemove) {
+  const addToCartSection = document.querySelector(".addToCart");
+
+  // Remove the selected lipstick div from the addToCartSection
+  addToCartSection.removeChild(selectedLipstickDiv);
+
+  // Remove the selected lipstick from the array
+  selectedLipsticksArray.splice(indexToRemove, 1);
+
+  // Update the stored array in localStorage
+  localStorage.setItem(
+    "selectedLipsticks",
+    JSON.stringify(selectedLipsticksArray)
+  );
+}
+
+function handleLipstickImageClick(img, icon, title, price) {
+  // Clear local storage and the UI if a lipstick is already selected
+  if (selectedLipsticksArray.length > 0) {
+    selectedLipsticksArray = [];
+    const addToCartSection = document.querySelector(".addToCart");
+    addToCartSection.innerHTML = "";  
+    localStorage.removeItem("selectedLipsticks"); // Clear local storage
+  }
+
+  // Call the function to display the selected lipstick in the addToCart section
+  displaySelectedLipstick(
+    img,
+    icon,
+    title,
+    price,
+    selectedLipsticksArray.length
+  );
+
+  // Add the selected lipstick data to the array
+  const selectedLipstickData = {
+    img: img,
+    icon: icon,
+    title: title,
+    price: price,
+  };
+  selectedLipsticksArray.push(selectedLipstickData);
+
+  // Store the updated array in localStorage
+  localStorage.setItem(
+    "selectedLipsticks",
+    JSON.stringify(selectedLipsticksArray)
+  );
+}
+
+function displaySelectedLipstickOnLoad() {
+  // Retrieve the selected lipsticks from localStorage
+  const storedLipsticks = localStorage.getItem("selectedLipsticks");
+  if (storedLipsticks) {
+    selectedLipsticksArray = JSON.parse(storedLipsticks);
+
+    // Display each selected lipstick in the addToCart section
+    selectedLipsticksArray.forEach((lipstickData, index) => {
+      displaySelectedLipstick(
+        lipstickData.img,
+        lipstickData.icon,
+        lipstickData.title,
+        lipstickData.price,
+        index
+      );
+    });
+  }
+}
+
+// Call the function on page load to display the stored lipsticks
+displaySelectedLipstickOnLoad();
+
 function generateMateLipstickContent() {
   const mateLipstickContainer = document.getElementById("mate");
-  // Map the dummy data and generate the HTML elements
+
   const mateLipstickHTML = lisptickMate
     .map(
       (item) => `
-    <div class="lipstickItem">
+    <div class="lipstickItem"  
+    onclick="handleLipstickImageClick( '${item.img}',
+    '${item.icon}', '${item.title}', '${item.price}')" >
       <img class="img" src=${item.img} />
       <img class="icon" src="${item.icon}"/>
       <h3>${item.title}</h3>
@@ -158,19 +294,16 @@ function generateMateLipstickContent() {
   mateLipstickContainer.innerHTML = mateLipstickHTML;
 }
 
-// Call the function to generate the Mate Lipstick content
 generateMateLipstickContent();
 
 function showScrollOnHover() {
-  const lipstickContainer = document.querySelector(".LipstickContainer");
-  const screenSizeThreshold = 1050; // Set your desired screen size threshold here
+  const lipstickContainer = document.querySelector(".LipstickContainerCategories");
+  const screenSizeThreshold = 1280;
 
   function updateScrollOnHover() {
     if (window.innerWidth <= screenSizeThreshold) {
-      // Show the scrollbar on hover for smaller screens
       lipstickContainer.style.overflowX = "scroll";
     } else {
-      // Hide the scrollbar for larger screens
       lipstickContainer.style.overflowX = "hidden";
     }
   }
@@ -187,17 +320,64 @@ function showScrollOnHovermate() {
   function updateScrollOnHover() {
     contentlipstick.style.overflowY = "scroll";
     contentlipstick.style.overflowY = "hidden";
-    
   }
 
   showScrollOnHovermate();
 
-  // Update scroll on window resize
   window.addEventListener("resize", updateScrollOnHover);
 }
+function showScrollOnHoverSidebar() {
+  const Side_Bar = document.querySelector(".Side_Bar");
+
+  function updateScrollOnHover() {
+    Side_Bar.style.overflowY = "scroll";
+    Side_Bar.style.overflowY = "hidden";
+  }
+
+  showScrollOnHoverSidebar();
+}
+
 // Call the function when the DOM is ready
-document.addEventListener("DOMContentLoaded", showScrollOnHover);
+document.addEventListener(
+  "DOMContentLoaded",
+  showScrollOnHover,
+  showScrollOnHovermate,
+  showScrollOnHoverSidebar
+);
 
+document.addEventListener("DOMContentLoaded", () => {
+  const sidebarMainContainer = document.querySelector(".Sidebar_MainContainer");
+  const toggleButton = document.createElement("div");
+  toggleButton.classList.add("toggle-button");
+  toggleButton.innerHTML = `
+    <div class="toggle-line"></div>
+    <div class="toggle-line"></div>
+    <div class="toggle-line"></div>
+  `;
 
+  document.body.appendChild(toggleButton);
 
+  toggleButton.addEventListener("click", () => {
+    sidebarMainContainer.classList.toggle("active");
+  });
+});
 
+// video = document.getElementById("videoElement");
+// if (navigator.mediaDevices.getUserMedia) {
+//   navigator.mediaDevices.getUserMedia({ video: true }).then(function (stream) {
+//     video.srcObject = stream;
+//   });
+// }
+
+// // run face-mesh model once the video is ready for processing
+// main();
+
+// function main() {
+//   // check if the video is loaded and ready for processing
+//   if (video.readyState == 4) {
+//     get_facemesh();
+//     sendFrame();
+//   } else {
+//     setTimeout(main, 1000 / 60);
+//   }
+// }
